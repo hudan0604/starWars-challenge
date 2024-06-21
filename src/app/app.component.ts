2098ddfd6d64f6edaa1ca1c00bef86c0335d6437
Nicolas Hudavert <hudanico@hotmail>
2024-06-21 15:05:51 +0200

    feat: design skeleton

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { data } from './shared/data/data';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [CommonModule, RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
 export class AppComponent {
   title = 'starWars-challenge';
+
+  public step = signal(0);
+  public data = data;
 }
